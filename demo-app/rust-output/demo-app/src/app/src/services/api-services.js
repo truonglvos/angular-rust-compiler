725f41320e9cdcf831e378b2b99bd48e3ff19bb9
truonglv4 <anhtruonglavm2@gmail.com>
2025-12-23 05:39:05 +0700

    feat(api-services): add ApiServices class and corresponding tests

---
+++ demo-app/rust-output/demo-app/src/app/src/services/api-services.js
@@ -0,0 +1,12 @@
+import * as i0 from "@angular/core";
+import { Injectable } from "@angular/core";
+export class ApiServices {
+	static ɵfac = function ApiServices_Factory(t) {
+		return new (t || ApiServices)();
+	};
+	static ɵprov = /* @__PURE__ */ i0.ɵɵdefineInjectable({
+		token: ApiServices,
+		factory: ApiServices.ɵfac,
+		providedIn: "root"
+	});
+}
