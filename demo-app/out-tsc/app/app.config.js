a6fae6452e831b09e86b04f9c0089c4252fae62f
truonglv4 <anhtruonglavm2@gmail.com>
2025-12-25 17:09:27 +0700

    chore: ngtsc output for zoneless

--- demo-app/out-tsc/app/app.config.js
+++ demo-app/out-tsc/app/app.config.js
@@ -1,10 +1,10 @@
-import { provideBrowserGlobalErrorListeners, provideZoneChangeDetection, } from '@angular/core';
+import { provideBrowserGlobalErrorListeners, provideZonelessChangeDetection, } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 export const appConfig = {
     providers: [
-        provideZoneChangeDetection({ eventCoalescing: true }),
         provideBrowserGlobalErrorListeners(),
+        provideZonelessChangeDetection(),
         provideRouter(routes),
     ],
 };
